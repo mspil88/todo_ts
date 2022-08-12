9d26333b92ca702c6a33ca878f8a32884d3c3493
mspil88 <matthewspillane88@gmail.com>
2022-08-12 12:54:41 +0100

    added todo list class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log("hi there")
+import {Todo, TodoList, Status, Importance} from "./todo";
+
+
+let test: Todo[] = [new Todo(1, "take out bins", "InProgress", "12/08/2021", "medium"),
+                    new Todo(2, "MOT", "InProgress", "14/08/2021", "high"),
+                    new Todo(3, "Tidy Kitchen", "InProgress", "15/08/2021", "low")]
+
+let list = new TodoList(test);
+console.log(list.getList())
+console.log(`Remaning Tasks ${list.countRemainingTasks()}`)
+list.taskCompleted(3);
+console.log(`Remaning Tasks ${list.countRemainingTasks()}`)
+list.taskCompleted(2);
+console.log(`Remaning Tasks ${list.countRemainingTasks()}`)
+
+// console.log(list.getList())
+// list.changeTaskAttribute(3, "importance", "High")
+// console.log(list.getList())
+// list.changeTaskAttribute(2, "name", "Sort MOT")
+// console.log(list.getList())
+// list.changeTaskAttribute(1, "date", "18/08/2021")
+// console.log(list.getList())
--- src/todo.ts
+++ src/todo.ts
@@ -25,18 +25,25 @@
 
 //consider getters setters in the todo list class instead of todo item?
 
-enum Status {
+export enum Status {
     Complete = "Complete",
     InProgress = "InProgress"
 }
 
-enum Importance {
+export enum Importance {
     High = "High",
     Medium = "Medium",
     Low = "Low"
 }
 
-class Todo {
+export enum TaskAttribute {
+    Date = "date",
+    Importance = "importance",
+    Name = "name",
+
+}
+
+export class Todo {
     constructor(private id: number, public name: string, public status: string, public todoDate: string, 
         public importance: string) {
             this.id = id;
@@ -51,6 +58,10 @@ class Todo {
                 status: this.status, 
                 todo_date: this.todoDate,
                 importance: this.importance}
+    }
+
+    getTaskId(): number {
+        return this.id;
     }
 
     markedCompleted(): void {
@@ -69,5 +80,46 @@ class Todo {
 
         const importanceVal : string = Importance[importance] !== undefined ? Importance[importance]: Importance.Medium
         this.importance = importanceVal
+    }
+}
+
+export class TodoList<T extends Todo> {
+    private tasksMap: Map<number, T>;
+
+    constructor(tasks: T[]) {
+        this.tasksMap = new Map();
+        tasks.forEach(task => this.tasksMap.set(task.getTaskId(), task));
+
+    }
+
+    getList(): Todo[] {
+        return [...this.tasksMap.values()];
+    }
+
+    getTaskById(id: number): Todo {
+        return this.tasksMap.get(id);
+    }
+
+    taskCompleted(id: number): void {
+        this.tasksMap.get(id).markedCompleted();
+    }
+
+    changeTaskAttribute(id: number, attribute: string, param: string): void {
+        switch(attribute) {
+            case TaskAttribute.Date:
+                this.tasksMap.get(id).setDate(param);
+                break;
+            case TaskAttribute.Importance:
+                this.tasksMap.get(id).setImportance(param);
+                break;
+            case TaskAttribute.Name:
+                this.tasksMap.get(id).setName(param);
+                break;}
+    }
+
+    countRemainingTasks(): number {
+        return [...this.tasksMap.values()]
+               .filter(task => task.status !== "Complete")
+               .length;
     }
 }
